342f54d2534c3176d0a722133e6b5e68c858c85b
Bogdan <98741979+sbacorp@users.noreply.github.com>
2023-12-23 07:43:54 +0300

    123

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,10 @@
 "use client";
 
-import Image from "next/image";
 import styles from "./page.module.scss";
-import { useLayoutEffect, useRef, useState } from "react";
 import Hero from "@/components/Hero";
-import {AnimatePresence, motion} from 'framer-motion'
+import { motion} from 'framer-motion'
 
  function Home() {
-	
-	const comp = useRef();
-	const [isLoading, setIsLoading] = useState(true);
-	useLayoutEffect(() => {
-		
-		setTimeout(() => {
-			setIsLoading(false);
-			document.body.style.cursor = 'default'
-			document.body.style.overflowY= 'auto'
-		}, 2000);
-	}, []);
     
 
 	return (
